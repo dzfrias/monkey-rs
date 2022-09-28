e58421d9e1e6498bbf43f578ee252c9df6b039c6
Diego <96022404+dzfrias@users.noreply.github.com>
2022-09-27 18:42:06 -0700

    feat: add REPL
    For now, it just prints the token stream generated from the user's
    input.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use monkey_rs::lexer::Lexer;
+use monkey_rs::token::Token;
+use std::io::{self, Write};
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello! This is the Monkey programming language");
+    println!("Feel free to type in commands");
+    start_repl();
+}
+
+fn start_repl() {
+    const PROMPT: &'static str = ">> ";
+
+    loop {
+        let mut repl_input = String::new();
+
+        print!("\n{PROMPT}");
+        io::stdout().flush().unwrap_or_else(|err| {
+            eprintln!("Internal REPL error when flushing stdout: {err}");
+            process::exit(1);
+        });
+        io::stdin()
+            .read_line(&mut repl_input)
+            .unwrap_or_else(|err| {
+                eprintln!("Internal REPL error when reading input: {err}");
+                process::exit(1);
+            });
+
+        let mut lexer = Lexer::new(&repl_input);
+        loop {
+            let token = lexer.next_token();
+            if let Token::EOF = token {
+                break;
+            }
+            println!("{:?}", token);
+        }
+    }
 }
