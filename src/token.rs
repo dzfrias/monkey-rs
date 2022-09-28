4d73170688bba47f19e6e77b2a88336a36f24517
Diego <96022404+dzfrias@users.noreply.github.com>
2022-09-27 19:58:05 -0700

    docs(token): add docs for token variants

--- src/token.rs
+++ src/token.rs
@@ -1,43 +1,64 @@
 /// A token in the Monkey language.
 #[derive(Debug, PartialEq)]
 pub enum Token {
+    /// An illegal character.
     Illegal,
+    /// A token for when the end of the input is reached.
     EOF,
 
-    // Identifiers
+    /// An identifier, supports all alphabetic unicode characters, "_", and numbers.
     Ident(String),
+    /// An integer.
     Int(String),
 
-    // Operators
+    /// An operator token represented by "=".
     Assign,
+    /// An operator token represented by "+".
     Plus,
+    /// An operator token represented by "-".
     Minus,
+    /// An operator token represented by "!".
     Bang,
+    /// An operator token represented by "*".
     Asterisk,
+    /// An operator token represented by "/".
     Slash,
 
-    // Comparison operators
+    /// An operator token represented by "<".
     Lt,
+    /// An operator token represented by ">".
     Gt,
+    /// An operator token represented by "==".
     Eq,
+    /// An operator token represented by "!=".
     NotEq,
 
-    // Delimiters
+    /// A token represented by ",".
     Comma,
+    /// A token represented by ";".
     Semicolon,
 
-    // Pairs
+    /// A token represented by "(".
     Lparen,
+    /// A token represented by ")".
     Rparen,
+    /// A token represented by "{".
     Lbrace,
+    /// A token represented by "}".
     Rbrace,
 
-    // Keywords
+    /// A keyword token represented by "fn".
     Function,
+    /// A keyword token represented by "let".
     Let,
+    /// A keyword token represented by "true".
     True,
+    /// A keyword token represented by "false".
     False,
+    /// A keyword token represented by "if".
     If,
+    /// A keyword token represented by "else".
     Else,
+    /// A keyword token represented by "return".
     Return,
 }
